6fdfcd81d18be217f5ec31ae05e884b6c51a054f
Maheeza12 <maheezadilthar1998@gmail.com>
2025-05-24 10:04:06 +0530

    Allowed orign Vercel

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ await connectDB()
 await connectCloudinary()
 
 //Allow multiple origins
-const allowedOrigins = ['http://localhost:5173', 'https://milliondeco.vercel.app']
+const allowedOrigins = ['http://localhost:5173', 'https://milliondecolk.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
